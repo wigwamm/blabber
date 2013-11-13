bfb123e53e1e5e6b68c7356ded80f53fa0c62973
Alex Hancock <alex@alexhancock.com>
2013-11-12 19:07:17 -0800

    [service] Foundations of a simple web service - mimicking the most basic functionality of proppycock

---
+++ service.js
@@ -0,0 +1,12 @@
+var connect = require('connect');
+var rest = require('connect-rest');
+var http = require('http');
+var app = connect();
+
+app.use(connect.bodyParser());
+app.use(rest.rester());
+
+var descriptionGenerator = require('./lib/descriptionGenerator');
+rest.post('/description', descriptionGenerator);
+
+http.createServer(app).listen(9000);
